1a9d8b2a5341bdcf33faae071b61d2f57164b5ae
varogonz95 <varogonz95@gmail.com>
2024-04-24 11:11:03 -0600

    fix(json): change serializer options

--- Pgotchi.Functions/Program.cs
+++ Pgotchi.Functions/Program.cs
@@ -1,14 +1,24 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Pgotchi.Functions;
+using System.Text.Json;
 
 var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
     .ConfigureFunctionsWebApplication(builder =>
     {
         builder.Services.AddOptions<AzureIotHubOptions>()
         .BindConfiguration(AzureIotHubOptions.SectionName)
         .ValidateDataAnnotations()
         .ValidateOnStart();
+    })
+    .ConfigureServices(services =>
+    {
+        services.Configure<JsonSerializerOptions>(config =>
+        {
+            config.PropertyNameCaseInsensitive = true;
+            config.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
     })
     .Build();
 
