e2f6bb4810623760dd31a87197634e48e4d0cea3
Álvaro González <varogonz95@gmail.com>
2024-04-24 13:00:12 -0600

    fix(program): remove configure worker defaults (#11)

--- Pgotchi.Functions/Program.cs
+++ Pgotchi.Functions/Program.cs
@@ -4,21 +4,12 @@ using Pgotchi.Functions;
 using System.Text.Json;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
     .ConfigureFunctionsWebApplication(builder =>
     {
         builder.Services.AddOptions<AzureIotHubOptions>()
         .BindConfiguration(AzureIotHubOptions.SectionName)
         .ValidateDataAnnotations()
         .ValidateOnStart();
-    })
-    .ConfigureServices(services =>
-    {
-        services.Configure<JsonSerializerOptions>(config =>
-        {
-            config.PropertyNameCaseInsensitive = true;
-            config.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        });
     })
     .Build();
 
