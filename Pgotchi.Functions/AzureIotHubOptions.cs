065e38f41728c48abfe7c3a54291be0eb351dfb6
Álvaro González <varogonz95@gmail.com>
2024-04-25 16:46:06 -0600

    dynamically change Json naming policy (#13)

--- Pgotchi.Functions/AzureIotHubOptions.cs
+++ Pgotchi.Functions/AzureIotHubOptions.cs
@@ -15,14 +15,3 @@ public sealed class AzureIotHubOptions
 
     public string? ApiVersion { get; set; } = string.Empty;
 }
-
-public static class AzureIotHubOptionsExtensions
-{
-    public static string ToDeviceUri(this AzureIotHubOptions instance, string deviceId)
-    {
-        ArgumentNullException.ThrowIfNull(deviceId);
-
-        return $"{instance.HostName}/{deviceId}"
-            + (string.IsNullOrEmpty(instance.ApiVersion) ? string.Empty : $"/?{instance.ApiVersion}");
-    }
-}
