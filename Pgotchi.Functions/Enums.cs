de798386f5ab13465106db01a3b42d8784ab47bd
Álvaro González <varogonz95@gmail.com>
2024-04-26 20:13:52 -0600

    feat(json): use unsafe json encoder (#14)
    feat(json): dynamically set propery naming policy
    refactor(register-device): code refactor

---
+++ Pgotchi.Functions/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace Pgotchi.Functions
+{
+    public enum ResponsePropertyNamingPolicy
+    {
+        CamelCase = 0,
+        SnakeCaseUpper = 1,
+        SnakeCaseLower = 2,
+        KebabCaseUpper = 3,
+        KebabCaseLower = 4,
+        CapitalCase = 5,
+    }
+}
